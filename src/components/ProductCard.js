cf30b2769ae490bb5a7d71bb484461b74fdb3aca
ahmedatef-hu <ahmed8482atef@gmail.com>
2026-04-05 07:28:29 -0700

    Remove unused useCart import

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { FiShoppingCart, FiHeart } from 'react-icons/fi';
-import { useCart } from '../context/CartContext';
 import { useAuth } from '../context/AuthContext';
 import ProductImage from './ProductImage';
 import api from '../utils/api';
